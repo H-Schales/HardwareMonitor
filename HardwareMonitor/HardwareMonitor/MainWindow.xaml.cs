625ee00c1b3b01154024f1b2086526a31a0dda4b
Hendrik <hendrik.schales@me.com>
2017-12-04 13:07:59 +0100

    using HardwareMonitorLib.dll to display CPU_Load and CPU_Temperature

--- HardwareMonitor/HardwareMonitor/MainWindow.xaml.cs
+++ HardwareMonitor/HardwareMonitor/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Management;
+using OpenHardwareMonitor.Collections;
+using OpenHardwareMonitor.Hardware;
+using System.Windows.Threading;
 
 namespace HardwareMonitor
 {
@@ -20,9 +24,42 @@ namespace HardwareMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
+        Computer thisComputer;
         public MainWindow()
         {
             InitializeComponent();
+            thisComputer = new Computer() { CPUEnabled = true };
+            thisComputer.Open();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) {
+            String temp = "";
+            String usage = "";
+
+            foreach (var hardwareItem in thisComputer.Hardware) {
+                if (hardwareItem.HardwareType == HardwareType.CPU) {
+                    hardwareItem.Update();
+                    foreach (IHardware subHardware in hardwareItem.SubHardware)
+                        subHardware.Update();
+
+                    foreach (var sensor in hardwareItem.Sensors) {
+                        if (sensor.SensorType == SensorType.Temperature) {
+                            temp += String.Format("{0} Temperature = {1}\r\n", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value");
+                        }
+                        if (sensor.SensorType == SensorType.Load) {
+                            usage += String.Format("{0} Load = {1}\r\n", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value");
+                        }
+                    }
+                }
+            }
+
+            CPU_temp.Text = temp;
+            CPU_usage.Text = usage;
         }
     }
 }
