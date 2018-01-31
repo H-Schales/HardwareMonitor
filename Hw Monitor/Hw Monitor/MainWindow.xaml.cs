1a52c7d98139384bac2d59ed203ce93d674b5cc3
Sebastian2205 <34235644+Sebastian2205@users.noreply.github.com>
2018-01-31 15:06:53 +0100

    Code Refactoring

--- Hw Monitor/Hw Monitor/MainWindow.xaml.cs
+++ Hw Monitor/Hw Monitor/MainWindow.xaml.cs
@@ -14,8 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Windows.Threading;
+using System.Net.NetworkInformation;
 using OpenHardwareMonitor.Hardware;
 using ZedGraph;
+using System.Management;
 
 namespace Hw_Monitor
 {
@@ -29,15 +31,23 @@ namespace Hw_Monitor
         PointPairList cpuLoadList = new PointPairList();
         PointPairList ramUsageList = new PointPairList();
         PointPairList diskUsageList = new PointPairList();
+        PointPairList networkList = new PointPairList();
+
+        //PerformanceCounter Netzwerkverkehr
+        PerformanceCounter network = new PerformanceCounter();
+        PerformanceCounter networkReceive = new PerformanceCounter();
+        PerformanceCounter networkSent = new PerformanceCounter();
+
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT totalphysicalmemory FROM Win32_ComputerSystem");
 
         //Werte der Hardware Komponenten um den Zed Graph zeichnen zu können
         double ramUsage = 0.0, diskUsage = 0.0, cpuLoad = 0.0;
-        int cpuTemperature = 0, x_time = 0;
+        int x_time = 0, network_Data = 0;
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
+        //Hardware Informationen
+        double totalMemory = 0;
+        int cpuTemperature = 0, diskTemperature = 0, networkDataReceive = 0, networkDataSent = 0;
 
-        }
 
         public MainWindow()
         {
@@ -62,16 +72,23 @@ namespace Hw_Monitor
             //zedGraph ramUsage
             zedgraph_ram.GraphPane.Title.Text = "Arbeitsspeicher";
             zedgraph_ram.GraphPane.XAxis.Title.Text = "Zeit";
-            zedgraph_ram.GraphPane.YAxis.Title.Text = "Arbeitsspeicherverbrauch in MB";
+            zedgraph_ram.GraphPane.YAxis.Title.Text = "verwendeter Arbeitsspeicher in %";
             zedgraph_ram.GraphPane.CurveList.Clear();
             ramUsageList.Clear();
 
             //zedGraph diskUsage
             zedgraph_disk.GraphPane.Title.Text = "Festplatte";
             zedgraph_disk.GraphPane.XAxis.Title.Text = "Zeit";
-            zedgraph_disk.GraphPane.YAxis.Title.Text = "Was wird hier überhaupt angezeigt Sebastian ? ";
+            zedgraph_disk.GraphPane.YAxis.Title.Text = "verwendeter Speicher";
             zedgraph_disk.GraphPane.CurveList.Clear();
             diskUsageList.Clear();
+
+            //ZedGraph NetworkTraffic
+            zedgraph_network.GraphPane.Title.Text = "Netzwerkverkehr";
+            zedgraph_network.GraphPane.XAxis.Title.Text = "Zeit";
+            zedgraph_network.GraphPane.YAxis.Title.Text = "Gesamtanzahl Bytes/s";
+            zedgraph_network.GraphPane.CurveList.Clear();
+            networkList.Clear();
         }
 
         private void button_click(object sender, RoutedEventArgs e)
@@ -96,6 +113,11 @@ namespace Hw_Monitor
                 textBox_diskInfo.Visibility = Visibility.Visible;
                 Zed_disk.Visibility = Visibility.Visible;
             }
+            else if (chosen == "Network")
+            {
+                textBox_networkInfo.Visibility = Visibility.Visible;
+                Zed_network.Visibility = Visibility.Visible;
+            }
         }
 
         public void timer_Tick(object sender, EventArgs e)
@@ -103,9 +125,32 @@ namespace Hw_Monitor
             String cpuLoadString = "";
             String ramUsageString = "";
             String hddUsageString = "";
-            String usage = "";
+            String cpuTempString = "";
             String cpuName = "";
+            String diskInfoString = "";
+            String string_NetworkStatus = "";
+            String string_MacAddress = "";
+            String userName = "";
+            String computerName = "";
+            String string_Network = "";
 
+            //Benutzername und Computernamen ermitteln
+            userName = Environment.UserName;
+            computerName = Environment.MachineName;
+
+           // Arbeitsspeichergröße ermitteln
+            ManagementObjectCollection res = searcher.Get();
+
+            foreach (ManagementObject mo in res)
+            {
+                totalMemory = long.Parse(mo["totalphysicalmemory"].ToString());
+            }
+
+            totalMemory = totalMemory / 1024 / 1024 / 1024;
+            totalMemory = Math.Round(totalMemory, 1);
+
+
+            //HardwareInformationen auslesen
             foreach (var hardwareItem in thisComputer.Hardware)
             {
                 if (hardwareItem.HardwareType == HardwareType.CPU)
@@ -119,34 +164,38 @@ namespace Hw_Monitor
                         if (sensor.SensorType == SensorType.Temperature)
                         {
                             cpuTemperature = (int)sensor.Value.Value;
-                            usage += String.Format("{0} Temperature = {1} °C\r\n", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value");
+                            cpuTempString += String.Format("{0} Temperature: {1} °C\r\n", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value");
                             checkCpuTemp(cpuTemperature);
                         }
 
                         if (sensor.SensorType == SensorType.Load)
                         {
                             cpuLoad = (double)sensor.Value.Value;
-                            cpuLoadString += String.Format("{0} Load = {1}\r\n", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value");
+                            cpuLoadString += String.Format("{0} Load: {1} %\r\n", sensor.Name, sensor.Value.HasValue ? Math.Round(sensor.Value.Value, 1).ToString() : "no value");
                         }
                     }
                 }
                 else if (hardwareItem.HardwareType == HardwareType.RAM) {
-                    hardwareItem.Update();
+                    hardwareItem.Update();                   
                     foreach (IHardware subHardware in hardwareItem.SubHardware)
                         subHardware.Update();
 
                     foreach (var sensor in hardwareItem.Sensors)
                     {    
                         if (sensor.SensorType == SensorType.Data)
+                        {                            
+                            ramUsageString += String.Format("{0} Ram:\t{1} GB\r\n", sensor.Name, sensor.Value.HasValue ? Math.Round(sensor.Value.Value, 1).ToString() : "no value");
+                        }
+                        else if (sensor.SensorType == SensorType.Load)
                         {
-                            ramUsage = (double)sensor.Value.Value;
-                            ramUsageString += String.Format("{0} Ram = {1}\r\n", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value");
+                            ramUsage = (double)sensor.Value.Value; 
                         }
                     }
                 }
                 else if (hardwareItem.HardwareType == HardwareType.HDD)
                 {
                     hardwareItem.Update();
+                    diskInfoString = hardwareItem.Name;
                     foreach (IHardware subHardware in hardwareItem.SubHardware)
                         subHardware.Update();
 
@@ -155,16 +204,77 @@ namespace Hw_Monitor
                         if (sensor.SensorType == SensorType.Load)
                         {
                             diskUsage = (double)sensor.Value.Value;
-                            hddUsageString += String.Format("{0} HDD = {1}\r\n", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value");
+                            hddUsageString += String.Format("{0} Disk:\t\t{1} GB\r\n", sensor.Name, sensor.Value.HasValue ? Math.Round(sensor.Value.Value, 1).ToString() : "no value");
+                        }
+                        else if (sensor.SensorType == SensorType.Data)
+                        {
+                            hddUsageString += String.Format("{0} Disk:\t{1} GB\r\n", sensor.Name, Math.Round(sensor.Value.Value, 1).ToString());
+                        }
+                        else if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            diskTemperature = (int)sensor.Value.Value;
                         }
                     }
                 }
+
+                //Netzwerk Informationen
+                foreach (var networkItem in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    string_Network = networkItem.Description.ToString();
+                    string_MacAddress = networkItem.GetPhysicalAddress().ToString();
+                    string_NetworkStatus = networkItem.OperationalStatus.ToString();
+
+                    if (string_NetworkStatus == "Down")
+                    {
+                        string_NetworkStatus = "";
+                        string_Network = "";
+                        string_MacAddress = "";
+                    }
+                    else
+                    {
+                        string_Network = string_Network.Replace("(", "[").Replace(")", "]");
+                        break;
+                    }
+                }
+
+                network.CategoryName = "Netzwerkschnittstelle";
+                network.CounterName = "Gesamtanzahl Bytes/s";
+                network.InstanceName = string_Network;
+                network_Data = (int)network.NextValue();
+
+                networkReceive.CategoryName = "Netzwerkschnittstelle";
+                networkReceive.CounterName = "Empfangene Bytes/s";
+                networkReceive.InstanceName = string_Network;
+                networkDataReceive += (int)networkReceive.NextValue();
+
+                networkSent.CategoryName = "Netzwerkschnittstelle";
+                networkSent.CounterName = "Bytes gesendet/s";
+                networkSent.InstanceName = string_Network;
+                networkDataSent = (int)networkSent.NextValue();
             }
 
             //Hardware Informationen
-            textBox_cpuInfo.Text = cpuName + "\n" + cpuLoadString + "\n" + usage;
-            textBox_ramInfo.Text = ramUsageString;
-            textBox_diskInfo.Text = hddUsageString;
+            textBox_cpuInfo.Text = "Prozessor:  " + cpuName + "\n" + 
+                                    cpuLoadString + "\n" + 
+                                    "Prozessortemperatur:" +"\n" + 
+                                    cpuTempString;
+            textBox_ramInfo.Text = "Arbeitsspeicher:" +"\n" + 
+                                   "Total Memory:" + "\t\t" + totalMemory + " GB\n" + 
+                                    ramUsageString;
+            textBox_diskInfo.Text = "Festplatte: " + "\n" +
+                                    "Bezeichnung:\t\t" + diskInfoString + "\n" +
+                                    hddUsageString +
+                                    "Temperatur:\t\t" + diskTemperature +"°C";
+            textBox_networkInfo.Text = "Netzwerkarte:" + "\t\t" + string_Network + "\n" +
+                                       "Gesamtanzahl Daten:" + "\t" + network_Data.ToString() + " Bytes/s\n" +
+                                       "Empfangene Daten:" + "\t" + networkDataReceive.ToString() + " Bytes/s\n" +
+                                       "Gesendete Daten:" + "\t\t" + networkDataSent.ToString() + " Bytes/s\n" + 
+                                       "Netzwerkstatus:" + "\t\t" + string_NetworkStatus + "\n" +
+                                       "Mac-Adresse" + "\t\t" + string_MacAddress;
+
+            //Label Informationen übergeben
+            LabelInfo.Content = "Benutzername: " + userName + "\n" +
+                                "Computer: " + computerName;
 
             //clear graph every 60 seconds
             clearZed();
@@ -173,13 +283,14 @@ namespace Hw_Monitor
             cpuLoadList.Add(x_time, cpuLoad);
             ramUsageList.Add(x_time, ramUsage);
             diskUsageList.Add(x_time, diskUsage);
+            networkList.Add(x_time, network_Data);
 
             //Graph zeichnen
-            LineItem myCurve_cpu = zedgraph_cpu.GraphPane.AddCurve("", cpuLoadList, System.Drawing.Color.Red, SymbolType.None);
+            LineItem myCurve_cpu = zedgraph_cpu.GraphPane.AddCurve("", cpuLoadList, System.Drawing.Color.Blue, SymbolType.None);
             zedgraph_cpu.AxisChange();
             zedgraph_cpu.Refresh();
 
-            LineItem myCurve_ram = zedgraph_ram.GraphPane.AddCurve("", ramUsageList, System.Drawing.Color.Blue, SymbolType.None);
+            LineItem myCurve_ram = zedgraph_ram.GraphPane.AddCurve("", ramUsageList, System.Drawing.Color.DarkRed, SymbolType.None);
             zedgraph_ram.AxisChange();
             zedgraph_ram.Refresh();
 
@@ -187,26 +298,36 @@ namespace Hw_Monitor
             zedgraph_disk.AxisChange();
             zedgraph_disk.Refresh();
 
-            LineItem myCurve_cpuMini = zedgraph_cpuMini.GraphPane.AddCurve("", cpuLoadList, System.Drawing.Color.Red, SymbolType.None);
+            LineItem myCurve_network = zedgraph_network.GraphPane.AddCurve("", networkList, System.Drawing.Color.DarkOrange, SymbolType.None);
+            zedgraph_network.AxisChange();
+            zedgraph_network.Refresh();
+
+            LineItem myCurve_cpuMini = zedgraph_cpuMini.GraphPane.AddCurve("", cpuLoadList, System.Drawing.Color.Blue, SymbolType.None);
             zedgraph_cpuMini.AxisChange();
             zedgraph_cpuMini.Refresh();
 
-            LineItem myCurve_ramMini = zedgraph_ramMini.GraphPane.AddCurve("", ramUsageList, System.Drawing.Color.Blue, SymbolType.None);
+            LineItem myCurve_ramMini = zedgraph_ramMini.GraphPane.AddCurve("", ramUsageList, System.Drawing.Color.DarkRed, SymbolType.None);
             zedgraph_ramMini.AxisChange();
             zedgraph_ramMini.Refresh();
 
             LineItem myCurve_diskMini = zedgraph_diskMini.GraphPane.AddCurve("", diskUsageList, System.Drawing.Color.Green, SymbolType.None);
             zedgraph_diskMini.AxisChange();
             zedgraph_diskMini.Refresh();
+
+            LineItem myCurve_networkMini = zedgraph_networkMini.GraphPane.AddCurve("", networkList, System.Drawing.Color.DarkOrange, SymbolType.None);
+            zedgraph_networkMini.AxisChange();
+            zedgraph_networkMini.Refresh();
         }
 
         public void everythingHidden() {
             textBox_cpuInfo.Visibility = Visibility.Hidden;
             textBox_ramInfo.Visibility = Visibility.Hidden;
             textBox_diskInfo.Visibility = Visibility.Hidden;
+            textBox_networkInfo.Visibility = Visibility.Hidden;
             Zed_cpu.Visibility = Visibility.Hidden;
             Zed_ram.Visibility = Visibility.Hidden;
             Zed_disk.Visibility = Visibility.Hidden;
+            Zed_network.Visibility = Visibility.Hidden;
         }
 
         public void clearZed() {
@@ -226,6 +347,11 @@ namespace Hw_Monitor
                 zedgraph_disk.GraphPane.CurveList.Clear();
                 zedgraph_diskMini.GraphPane.CurveList.Clear();
                 diskUsageList.Clear();
+
+                //Network
+                zedgraph_network.GraphPane.CurveList.Clear();
+                zedgraph_networkMini.GraphPane.CurveList.Clear();
+                networkList.Clear();
 
                 x_time = 0;
             }
